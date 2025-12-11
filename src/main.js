42ee8341dfed17e57f57509bacc46c25714a6331
Kristian Gøystdal <krisgoy100@gmail.com>
2025-12-11 22:42:37 +0100

    refactor i18n setup to dynamically load language files and streamline initialization

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import { auth } from "./js/firebaseConfig.js";
 import { onAuthStateChanged } from "firebase/auth";
 import vuetify from "./plugins/vuetify";
 import router from "./js/router.js";
-import { createI18n } from "vue-i18n";
-import en from "./languages/en.json";
-import no from "./languages/no.json";
-import { i18nMixin } from "./mixins/i18nMixin"; // Import the mixin
 import Vue3Toastify from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import i18n from "./js/i18n.js";
+import { i18nMixin } from "./mixins/i18nMixin";
 
 import "./assets/css/main.css";
 import "./assets/css/bg.css";
@@ -26,21 +24,6 @@ import "./assets/css/datatable.css";
 import "./assets/css/expansion-panel.css";
 import "./assets/css/profile-card.css";
 import "./assets/css/custom-toastify.css";
-
-// Set up messages for vue-i18n
-const messages = {
-	en,
-	no,
-};
-
-// Create an i18n instance with the locale configuration
-const i18n = createI18n({
-	legacy: false,
-	globalInjection: true,
-	locale: localStorage.getItem("language") || "no", // default locale
-	fallbackLocale: "no", // fallback locale
-	messages,
-});
 
 let app;
 
