927638865fcc38fb6d9e1a13152a64bae9e46038
Kristian Gøystdal <krisgoy100@gmail.com>
2026-06-18 14:08:33 +0200

    fix: replace hardcoded Firebase database URL with environment variable

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -10,8 +10,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  databaseURL: 'https://utveksling-database-default-rtdb.europe-west1.firebasedatabase.app',
-
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
