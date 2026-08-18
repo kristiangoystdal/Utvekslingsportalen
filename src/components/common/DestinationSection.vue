e2b67458425f105c40d538a76178438cf2e5c360
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2026-08-18 19:51:18 +0200

    Fix layout issues and update column classes for consistent padding (#246)

--- src/components/common/DestinationSection.vue
+++ src/components/common/DestinationSection.vue
@@ -1,25 +1,25 @@
 <template>
-	<v-row>
-		<v-col cols="12" md="6">
+	<v-row no-gutters>
+		<v-col cols="12" md="6" class="pa-1">
 			<v-autocomplete :model-value="country" :items="countryItems" item-title="name" item-value="key"
 				:label="$t('database.country')" variant="outlined" density="compact" clearable
 				:hint="$t('hints.country')" persistent-hint autocomplete="off"
 				@update:model-value="onCountryChange" />
 		</v-col>
-		<v-col cols="12" md="6">
+		<v-col cols="12" md="6" class="pa-1">
 			<v-autocomplete :model-value="university" :items="universityItems" :label="$t('database.university')"
 				:disabled="!country" variant="outlined" density="compact" clearable
 				:hint="!country ? $t('hints.selectCountryFirst') : $t('hints.university')"
 				persistent-hint autocomplete="off"
 				@update:model-value="$emit('update:university', $event)" />
 		</v-col>
-		<v-col cols="12" md="6">
+		<v-col cols="12" md="6" class="pa-1">
 			<v-autocomplete :model-value="numSemesters" :items="[1, 2]" :label="$t('database.numSemesters')"
 				variant="outlined" density="compact" clearable :hint="$t('hints.numSemesters')"
 				persistent-hint autocomplete="off"
 				@update:model-value="$emit('update:numSemesters', $event)" />
 		</v-col>
-		<v-col v-if="numSemesters === 1" cols="12" md="6">
+		<v-col v-if="numSemesters === 1" cols="12" md="6" class="pa-1">
 			<v-autocomplete :model-value="semester" :items="['Høst', 'Vår', 'Sommer']" :label="$t('database.semester')"
 				variant="outlined" density="compact" clearable :hint="$t('hints.semester')"
 				persistent-hint autocomplete="off"
